6b9f0be1ecee64d20a0ee31fddcbeba71fe1ee25
Tech-Morph <205584457+Tech-Morph@users.noreply.github.com>
2026-02-11 02:19:46 -0800

    Update send-payload.php

--- send-payload.php
+++ send-payload.php
@@ -7,17 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         http_response_code(404);
         die("Error: Payload not found!");
     }
-
-    // Send payload to the PS4 via socket
-    $ps4_ip = "127.0.0.1"; // Change to your PS4's actual IP!
-    $ps4_port = 9090; // Default GoldHEN Bin Loader port
+    
+    $ps4_ip = "127.0.0.1";
+    $ps4_port = 9090;
 
     $socket = fsockopen($ps4_ip, $ps4_port, $errno, $errstr, 5);
     if (!$socket) {
         die("Error: Could not connect to PS4 ($errno: $errstr)");
     }
 
-    // Open the binary payload and send it
     $payloadData = file_get_contents($filePath);
     fwrite($socket, $payloadData);
     fclose($socket);
